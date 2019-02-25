7435c387a6d062a211e88c2f6ef844f9375dd3c3
Don Park <donp@donp.org>
2019-02-25 14:48:21 -0800

    openpixel parse

--- lua/crystal/openpixel.lua
+++ lua/crystal/openpixel.lua
@@ -8,17 +8,26 @@ function openpixel.go(buff, payload)
       buff = buff .. payload
     end
   end
-  if buff:len() >= 4 then
-    openpixel.packet(buff)
+  bufflen = buff:len()
+  if bufflen >= 4 then
+    channel, command, msglen = openpixel.header(buff)
+    if bufflen-4 >= msglen then
+      openpixel.dispatch(channel, command, buff:sub(5, bufflen))
+    end
   end
 end
 
-function openpixel.packet(buff)
-  log("openpixel buff "..buff:len())
+function openpixel.header(buff)
+  log("openpixel header "..buff:len())
   channel = buff:sub(1,1)
   command = buff:sub(2,2)
   len = (buff:sub(3,3):byte()*256) + buff:sub(4,4):byte()
   log("channel "..channel:byte())
   log("command "..command:byte())
   log("payload len "..len)
+  return channel, command, len
+end
+
+function openpixel.dispatch(channel, command, buff)
+  log("openpixel dispatch "..channel:byte().." "..command:byte().." "..buff:len())
 end
