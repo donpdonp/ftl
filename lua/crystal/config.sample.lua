5e535e48f100fd5604601de2c29504554d7d438b
Don Park <donp@donp.org>
2019-02-26 11:15:09 -0800

    config update

--- lua/crystal/config.sample.lua
+++ lua/crystal/config.sample.lua
@@ -2,8 +2,10 @@ log("*ftl.config")
 ftl.config = {}
 
 -- wifi settings
-ftl.config.wifi = { ssid = "Bob",
-                    pwd = "zoo" }
+ftl.config.wifi = { ssid = "myap",
+                    pwd = "pass" }
 
 ftl.config.pixels = { count = 360,
-                      bytes = 4 }
+                      bytesperpixel = 4,
+                      datapin = 7,
+                      clockpin = 5 }
